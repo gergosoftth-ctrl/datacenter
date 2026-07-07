360ce0c85b074ea7740757ae59c939e89598c1a6
gergosoftth-ctrl <gergosoftth@gmail.com>
2026-07-07 14:54:00 +0700

    Update main.py
    update text

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import streamlit as st
 # ตั้งค่าหน้าจอ Dashboard
 st.set_page_config(page_title="Data Center Dashboard", layout="wide")
 
-st.sidebar.title("🚀 ศูนย์รวมแอปพลิเคชัน")
-st.sidebar.write("เลือกเครื่องมือที่ต้องการใช้งานด้านล่าง:")
+st.sidebar.title("Data Center Tools")
+st.sidebar.write("เลือก Tool ที่ต้องการใช้งานด้านล่าง:")
 
 # เมนูสำหรับเลือกแอป (ในอนาคตสามารถมาเพิ่มรายชื่อตรงนี้ได้เลย)
 app_mode = st.sidebar.radio(
-    "แอปที่มีให้ใช้งาน:",
-    ["🧹 เครื่องมือจัดการข้อความ (Text Cleaner)", "➕ แอปอื่น ๆ ในอนาคต"]
+    "Tools:",
+    ["🧹 ร่างข้อความ incident", "➕ แอปอื่น ๆ ในอนาคต"]
 )
 
 # เรียกใช้งานแอปตามที่ยูสเซอร์เลือก
-if app_mode == "🧹 เครื่องมือจัดการข้อความ (Text Cleaner)":
+if app_mode == "🧹 ร่างข้อความ incident":
     # ดึงโค้ดจากไฟล์แอปย่อยมาทำงาน
     from apps import text_cleaner
     text_cleaner.run_app()
     
 elif app_mode == "➕ แอปอื่น ๆ ในอนาคต":
     st.title("🚧 กำลังพัฒนา")
-    st.write("พื้นที่นี้รองรับสำหรับแอปพลิเคชันใหม่ของคุณในอนาคต สามารถเพิ่มเข้ามาได้เรื่อย ๆ ครับ!")
+    st.write("รอไปก่อนตอนนี้ขี้เกียจ zZ")
